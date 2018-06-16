c40564a82e7d827325fe6d8130226a1cb8fabd4c
codebuild <info@deep-impact.ch>
2018-06-16 19:00:00 +0200

    Brew formula update for grt version v0.2.5

--- grt.rb
+++ grt.rb
@@ -1,9 +1,9 @@
 class Grt < Formula
   desc "CLI tool for AWS MFA credentials"
   homepage "https://github.com/DEEP-IMPACT-AG/skuld"
-  url "https://github.com/stanislas/grt/releases/download/v0.2.0/grt_0.2.0_darwin_amd64.tar.gz"
-  version "0.2.0"
-  sha256 "cff5a2bd8a4fa9524e647e12f2084d507140333d86d93bad152820a52c5b4bfb"
+  url "https://github.com/stanislas/grt/releases/download/v0.2.5/grt_0.2.5_darwin_amd64.tar.gz"
+  version "0.2.5"
+  sha256 "e8b219f58d3d982c45616f0ced70367fd268b352ae533cec5a9fd2fb839440e1"
 
   def install
     bin.install "grt"
